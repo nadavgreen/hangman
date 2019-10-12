060428996a0a61c0f4eae6755a14c972a966ca99
Nadav Green <nadavgreen@Nadavs-MBP.fios-router.home>
2019-10-12 07:45:36 -0400

    Initial push of hangman_microservices to Heroku

---
+++ app.py
@@ -0,0 +1,39 @@
+# External Modules
+from flask import Flask, jsonify, request
+
+#Internal Modules
+from game_modules.game_class import Game
+
+app = Flask(__name__)
+
+@app.route('/ping', methods=['GET'])
+def pong():
+	if request.method == 'GET':
+		pass
+		return jsonify({'msg': 'pong'})
+
+@app.route('/', methods=['POST'])
+def game():
+	if request.method == 'POST':
+		pass
+		req = request.get_json()
+
+		game = None
+		res = {
+			'msg': None
+		}
+
+		try:
+			game = Game.check(**req)
+		except TypeError as err:
+			res['msg'] = str(err)
+		except ValueError as err:
+			res['msg'] = str(err)
+		else:
+			res['msg'] = game
+
+		return jsonify(res)
+
+if __name__ == '__main__':
+	app.debug = True
+	app.run()
---
+++ game_modules/game_class.py
@@ -0,0 +1,65 @@
+class Game:
+	@staticmethod
+	def check(guess, guesses, random_word):
+		pass
+		Game._check_random_word(random_word)
+		Game._check_guess(guess, random_word)
+		Game._check_guesses(guesses, random_word)
+
+		guesses.extend(letter for letter in guess if letter not in guesses)
+		lives, solved = Game._remaining_lives(guesses, random_word)
+
+		return {
+			'lives': lives,
+			'guesses': guesses,
+			'random_word': random_word,
+			'solved': solved
+		}
+
+	@staticmethod
+	def _check_guess(guess, random_word):
+		pass
+		if type(guess) != str:
+			raise TypeError('type of guess or guess in guesses must be a str')
+		if not len(guess):
+			raise ValueError('length of guess or guess in guesses must be at least 1')
+		if len(guess) > len(random_word):
+			raise ValueError('length of guess or guess in guesses must be less than or equal to length of word')
+		if not str.isalpha(guess):
+			raise ValueError('value of guess or guess in guesses must be alpha')
+
+	@staticmethod
+	def _check_guesses(guesses, random_word):
+		pass
+		if type(guesses) != list:
+			raise TypeError('type of guesses must be a list')
+		for guess in guesses:
+			Game._check_guess(guess, random_word)
+
+	@staticmethod
+	def _check_random_word(random_word):
+		pass
+		if type(random_word) != str:
+			raise TypeError('type of random_word must be a str')
+		if not len(random_word):
+			raise ValueError('length of random_word must be at least 1')
+		if not str.isalpha(random_word):
+			raise ValueError('value of random_word must be alpha')
+
+	@staticmethod
+	def _remaining_lives(guesses, random_word):
+		lives = 6
+		solved = True
+		
+		for guess in guesses:
+			if guess not in random_word:
+				lives -= 1
+			if not lives:
+				break
+
+		for letter in random_word:
+			if letter not in guesses:
+				solved = False
+				break
+
+		return lives, solved
